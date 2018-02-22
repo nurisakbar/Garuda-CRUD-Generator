321e32684368a3508b729802944e71aa81eab0a1
Izharul Haq <atoz.chevara@yahoo.com>
2018-02-22 09:41:00 +0700

    Update create_view_read.php
    Fix typo

--- harviacode/core/create_view_read.php
+++ harviacode/core/create_view_read.php
@@ -1,6 +1,6 @@
-<?php 
-
-$string = "<div class=\"content-wrapper\">
+<?php
+$string = "
+<div class=\"content-wrapper\">
 	
 	<section class=\"content\">
 		<div class=\"box box-warning box-solid\">
@@ -8,24 +8,25 @@ $string = "<div class=\"content-wrapper\">
 				<h3 class=\"box-title\">DETAIL DATA ".  strtoupper($table_name)."</h3>
 			</div>
 		
-		<table class='table table-bordered>'        
+		<table class='table table-bordered'>        
 ";
 foreach ($non_pk as $row) {
-	$string .= "\n\t<tr>
+	$string .= "\n\t
+			<tr>
 				<td>".label($row["column_name"])."</td>
 				<td><?php echo $".$row["column_name"]."; ?></td>
 			</tr>";
 }
-$string .= "\n\t<tr>
+$string .= "\n\t
+			<tr>
 				<td></td>
 				<td><a href=\"<?php echo site_url('".$c_url."') ?>\" class=\"btn btn-default\">Kembali</a></td>
 			</tr>";
 $string .= "\n\t
 		</table>
 		</div>
-	</div>
+	</section>
 </div>";
 
 $hasil_view_read = createFile($string, $target."views/" . $c_url . "/" . $v_read_file);
-
 ?>
