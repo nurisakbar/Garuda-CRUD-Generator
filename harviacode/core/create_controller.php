eae0d35ce65e28be6117592057690c83ab018f88
Nuris Akbar <nuris.akbar@gmail.com>
2023-06-20 06:27:44 +0700

    Update create_controller.php

--- harviacode/core/create_controller.php
+++ harviacode/core/create_controller.php
@@ -10,7 +10,7 @@ class " . $c . " extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        is_login();
+        //is_login();
         \$this->load->model('$m');
         \$this->load->library('form_validation');";
 
