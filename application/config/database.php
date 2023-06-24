3bd8fe41fb79a5c9d4000ba155b037356aea9192
Nuris Akbar <nuris.akbar@gmail.com>
2023-06-24 15:15:23 +0700

    add support env

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'cigenerator',
-	'dbdriver' => 'mysqli',
+	'hostname' => getenv('DB_HOSTNAME'),
+	'username' => getenv('DB_USERNAME'),
+	'password' => getenv('DB_PASSWORD'),
+	'database' => getenv('DB_DATABASE'),
+	'dbdriver' => getenv('DB_DRIVER'),
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+# Load phpdotenv
+$hook['pre_system'] = function() {
+    $dotenv = Dotenv\Dotenv::create(FCPATH);
+    $dotenv->load();
+   };
